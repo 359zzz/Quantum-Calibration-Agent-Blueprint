89811ae3d7d071111ec02f06277904b4bc9acd94
mitchdz <mitch_dz@hotmail.com>
2026-04-09 12:51:15 -0700

    restrict allowed methods
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- server.py
+++ server.py
@@ -188,7 +188,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # explicit allowed origins
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "DELETE"],
     allow_headers=["*"],
 )
 
